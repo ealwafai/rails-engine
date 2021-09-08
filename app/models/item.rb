4a9786bd382fa3588d0a47bbf5e395cc0aec0936
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-09-08 14:42:32 -0500

    Complete item search

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,22 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+
+  validates :name, :description, :unit_price, presence: true
+  validates :unit_price, numericality: { greater_than: 0.0 }
+
+  def self.search_by_name(query)
+    var = where('name ilike ?', "%#{query}%")
+    .order(Arel.sql('lower(name) desc')).to_a
+  end
+
+  def self.search_by_price(price)
+    if price[:min_price] && price[:max_price]
+      where('unit_price > ?', price[:min_price]).where('unit_price < ?', price[:max_price]).to_a
+    elsif price[:max_price]
+      where('unit_price < ?', price[:max_price]).to_a
+    elsif price[:min_price]
+      where('unit_price > ?', price[:min_price]).to_a
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/merchants/find', to: 'merchants/search#find'
+      get '/items/find_all', to: 'items/search#find_all'
       resources :merchants, only: [:index, :show]
       resources :items, except: [:new, :edit]
     end

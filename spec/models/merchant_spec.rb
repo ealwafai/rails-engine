5ec99e33c5d6e28bed3810b2c06dbc85908f749e
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-09-08 10:17:12 -0500

    Add test for class method find by name

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -5,4 +5,23 @@ RSpec.describe Merchant do
     it { should have_many(:items) }
     it { should have_many(:invoices) }
   end
+  describe 'class methods' do
+    describe 'find_by_name' do
+      it 'returns the first name in case insensitive, and alphabetical order' do
+        merchant_1 = create(:merchant, name: 'Matt')
+        merchant_2 = create(:merchant, name: 'Brett')
+        merchant_3 = create(:merchant, name: 'Brian')
+
+        expect(Merchant.find_by_name('Br')).to eq merchant_2
+      end
+
+      it 'returns an empty array if no matches found' do
+        merchant_1 = create(:merchant, name: 'Matt')
+        merchant_2 = create(:merchant, name: 'Brett')
+        merchant_3 = create(:merchant, name: 'Brian')
+
+        expect(Merchant.find_by_name('Sam')).to be_nil
+      end
+    end
+  end
 end

9b9e880ea3b06c2712a62daf3ede5c9937e1585c
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-09-08 22:17:56 -0500

    Complete merchant items

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/merchants/find', to: 'merchants/search#find'
       get '/items/find_all', to: 'items/search#find_all'
+
+      get '/merchants/:id/items', to: 'merchants/items#index'
+      
       resources :merchants, only: [:index, :show]
       resources :items, except: [:new, :edit]
     end
